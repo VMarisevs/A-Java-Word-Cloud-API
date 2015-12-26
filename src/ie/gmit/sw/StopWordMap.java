aa8c3ce41f9c51a032f173afe944df8080e3db99
Vladislavs Marisevs <vmarisevs@gmail.com>
2015-12-26 23:49:55 +0000

    delegate contains key method

--- src/ie/gmit/sw/StopWordMap.java
+++ src/ie/gmit/sw/StopWordMap.java
@@ -4,9 +4,10 @@ import java.io.*;
 import java.util.*;
 
 public class StopWordMap {
-
+	
 	private Map<String, Integer> map = new HashMap<String, Integer>();
 	
+	// populating map with words
 	public void load(String filename) throws IOException{
 		
 		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(filename)));
@@ -19,7 +20,15 @@ public class StopWordMap {
 		bufferedReader.close();
 	}
 	
+	// delegate job to the map
+	public boolean containsKey(Object key) {
+		return map.containsKey(key);
+	}
 	
+	
+	/*
+	 * this part is just for class testing
+	 */
 	private void display(){
 		for (Map.Entry<String, Integer> word : map.entrySet()){
 			System.out.println(word.getKey() + " " + word.getValue());
@@ -31,5 +40,7 @@ public class StopWordMap {
 		StopWordMap swm = new StopWordMap();
 		swm.load("./stopwords.txt");
 		swm.display();
+		
+		System.out.println(swm.containsKey(new String("until")));;
 	}
 }
