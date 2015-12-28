9942e165fa1a98488bc6e2796de36f4e2d442bec
Vladislavs Marisevs <vmarisevs@gmail.com>
2015-12-28 13:37:30 +0000

    creating a word cloud in stack

--- src/ie/gmit/sw/DrawWordCloud.java
+++ src/ie/gmit/sw/DrawWordCloud.java
@@ -1,7 +1,5 @@
 package ie.gmit.sw;
 
-import java.awt.Color;
-import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -12,25 +10,28 @@ import javax.imageio.ImageIO;
 public class DrawWordCloud {
 	private BufferedImage image;
 	Graphics graphics;
-	
-	public DrawWordCloud(){
-		image = new BufferedImage(600, 10000, BufferedImage.TYPE_4BYTE_ABGR);
+		
+	public void drawWordCloudImage(Word[] words, int width, int height){
+		
+		// init new buffered image
+		image = new BufferedImage(width, height, BufferedImage.TYPE_4BYTE_ABGR);
 		graphics = image.getGraphics();
-	}
-	
-	public void drawWordCloudImage(Word[] words){
 		
+		/*
+		 * This code will display the words in descending order, in the list view
+		 */
 		
-		// graphics.drawString(words[0].getWord(), 0, 35);
-		int posy = 0;
-		for (int i = words.length-1, j = 0; i > 0 ; i-- , j++){
-			int fontSize = words[i].getFrequency()* 25;
-			posy += words[i].getFrequency()* 25;
-			Font font = new Font(Font.SANS_SERIF, Font.PLAIN, words[i].getFrequency()* 25 );
-			graphics.setColor(Color.red);
-			graphics.setFont(font);
-			graphics.drawString(words[i].getWord(), 0 , posy);
-			System.out.println(words[i].getWord() + ":" + words[i].getFrequency());
+		int y = 0;
+		
+		for (int i = 0; i < words.length; i++){
+			
+			words[i].setFont(words[words.length-1].getFrequency());
+			
+			graphics.setColor(words[i].getColor());
+			graphics.setFont(words[i].getFont());
+			
+			y += words[i].getFontSize();
+			graphics.drawString(words[i].getWord(), 0 , y);
 		}
 		
 	}
@@ -46,7 +47,7 @@ public class DrawWordCloud {
 		Word[] words = wcm.generate("./assignment-description.txt");
 		
 		DrawWordCloud dwc = new DrawWordCloud();
-		dwc.drawWordCloudImage(words);
+		dwc.drawWordCloudImage(words,1000,5000);
 		dwc.save("image");
 	}
 }
--- src/ie/gmit/sw/Word.java
+++ src/ie/gmit/sw/Word.java
@@ -1,9 +1,24 @@
 package ie.gmit.sw;
 
+import java.awt.*;
+import java.util.Random;
+
 public class Word {
 
+	public static int MAX_FONT_SIZE = 100;
+	public static int[] FONT_TYPE = {Font.PLAIN, Font.ITALIC, Font.BOLD};
+	public static String[] FONT_STYLE = {Font.SANS_SERIF, Font.MONOSPACED};
+	public static Color[] TEXT_COLOR = {Color.BLACK, Color.BLUE, Color.CYAN, Color.DARK_GRAY
+			, Color.GRAY, Color.GREEN, Color.LIGHT_GRAY, Color.MAGENTA, Color.ORANGE, Color.PINK
+			, Color.RED, Color.WHITE, Color.YELLOW};
+	
+	
 	private String word;
 	private int frequency;
+	private Font font;
+	private Color color;
+	private int fontSize;
+	private int textWidth;
 	
 	public Word(String _word){
 		word = _word;
@@ -27,4 +42,38 @@ public class Word {
 		return word;
 	}
 	
+	public void setFont(int maxFreq){
+		
+		fontSize = (int)((float)frequency / (float)maxFreq * Word.MAX_FONT_SIZE);
+		
+		Random random = new Random();
+		
+		int fontType = random.nextInt(Word.FONT_TYPE.length);
+		int fontStyle = random.nextInt(Word.FONT_STYLE.length);
+		
+		font = new Font(Word.FONT_STYLE[fontStyle], Word.FONT_TYPE[fontType] , fontSize);
+		
+		int textColor = random.nextInt(Word.TEXT_COLOR.length);
+		color = Word.TEXT_COLOR[textColor];
+	}
+	
+	public Font getFont(){
+		return font;
+	}
+	
+	public int getFontSize(){
+		return fontSize;
+	}
+	
+	public Color getColor(){
+		return color;
+	}
+
+	public void setTextWidth(int textWidth){
+		this.textWidth = textWidth;
+	}
+	
+	public int getTextWidth(){
+		return textWidth;
+	}
 }
